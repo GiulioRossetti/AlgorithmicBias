34db0bb1c74cbd16175deda9f87a4dcb2281b256
ValentinaPansanella <valentina.pansanella@gmail.com>
2021-08-03 22:43:48 +0200

    main.jl updated media_op = [0.05, 0.5, 0.95]

--- main.jl
+++ main.jl
@@ -21,12 +21,14 @@ end
 function multiple_runs(f, name, params, nsteady; nruns)
     finalops = []
     final_clusters = []
+    println("starting $nruns runs for params = $params")
     for nr in 1:nruns
+        println("run $nr")
         r =  f(params... ; nsteady=nsteady)
         df = DataFrame(r)
         #println(experiment_name)
-        CSV.write("res/$name.csv",  df, header=false)
-        spaghetti_plot(df, size(r)[1], "plots/$name.png")
+        CSV.write("res/$name nr$nr.csv",  df, header=false)
+        spaghetti_plot(df, size(r)[1], "plots/$name nr$nr.png")
 
         # aggregate stats
         fo = r[size(r)[1]]
@@ -35,6 +37,8 @@ function multiple_runs(f, name, params, nsteady; nruns)
         append!(final_clusters, clusters)
         append!(finalops, [fo])
     end
+    println("ending $nruns runs for params = $params")
+
     return finalops, final_clusters
 end
 
@@ -42,37 +46,37 @@ end
 
 
 # Graph initialization
-n = 250
+n = 100
 p = 1.0
 g = erdos_renyi(n, p)
 
 # simulation global parameters
-max_t = 100
-nsteady = 50
+max_t = 1000000
+nsteady = 1000
 
 
-# Example Deffuant
-for ϵ in [0.1, 0.2, 0.3, 0.4]
-    params = [g, ϵ, max_t]
-    name = "deffuant e$ϵ mi$max_t"
-    final_opinions, final_clusters = multiple_runs(deffuant, name, params, nsteady; nruns=10)
-    write_aggregate(name, final_opinions, final_clusters)
-end
+# # Example Deffuant
+# for ϵ in [0.1, 0.2, 0.3, 0.4]
+#     params = [g, ϵ, max_t]
+#     name = "deffuant e$ϵ mi$max_t"
+#     final_opinions, final_clusters = multiple_runs(deffuant, name, params, nsteady; nruns=10)
+#     write_aggregate(name, final_opinions, final_clusters)
+# end
 
 
-# Example Bias
-for ϵ in [0.1, 0.2, 0.3, 0.4], γ in [0, 1, 1.5, 2]
-    name = "bias e$ϵ g$γ mi$max_t"
-    params = [g, ϵ, γ, max_t]
-    final_opinions, final_clusters = multiple_runs(deffuant_bias, name, params, nsteady; nruns=10)
-    write_aggregate(name, final_opinions, final_clusters)
-end
+# # Example Bias
+# for ϵ in [0.1, 0.2, 0.3, 0.4], γ in [0, 1, 1.5, 2]
+#     name = "bias e$ϵ g$γ mi$max_t"
+#     params = [g, ϵ, γ, max_t]
+#     final_opinions, final_clusters = multiple_runs(deffuant_bias, name, params, nsteady; nruns=10)
+#     write_aggregate(name, final_opinions, final_clusters)
+# end
 
 # Example Media
-media_op = [0.1, 0.5, 0.9]
-for ϵ in [0.1, 0.2, 0.3, 0.4], γ in [0, 1, 1.5, 2], γₘ in  [0, 1, 1.5, 2],  pₘ in [0.1, 0.2, 0.3, 0.4]
+media_op = [0.05, 0.5, 0.95]
+for ϵ in [0.5, 0.3, 0.2], γ in [0, 0.5, 0.75, 1, 1.25, 1.5], γₘ in  [0, 0.5, 0.75, 1, 1.25, 1.5],  pₘ in [0.0, 0.2, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0]
     params = [g, ϵ, γ, γₘ, pₘ, media_op, max_t]
     name = "media e$ϵ g$γ gm$γ p$pₘ mi$max_t"
-    final_opinions, final_clusters = multiple_runs(deffuant_bias_media, name, params, nsteady; nruns=10)
+    final_opinions, final_clusters = multiple_runs(deffuant_bias_media, name, params, nsteady; nruns=100)
     write_aggregate(name, final_opinions, final_clusters)
 end
