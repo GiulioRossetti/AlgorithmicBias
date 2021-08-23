ec4fa0a16fdd30273c78a06e897a6354085d88c2
ValentinaPansanella <valentina.pansanella@gmail.com>
2021-08-24 01:54:48 +0200

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,44 +1,65 @@
+include("execution.jl")
 include("deffuant.jl")
 include("bias.jl")
 include("media.jl")
+include("analysis.jl")
 
 using CSV
 using DataFrames
 using JSON
 using LightGraphs
 
-function write_aggregate(name, final_opinions, final_clusters)
+# Graph initialization
+n = 100
+p = 1.0
+g = erdos_renyi(n, p)
 
-    df = DataFrame(final_opinions)
-    CSV.write("aggregate/final_opinions $name.txt",  df, header=false)
+# simulation global parameters
+max_t = 1000000
+nsteady = 1000
 
-    json_string = JSON.json(final_clusters)
-    open("aggregate/final_clusters $name.json","w") do f
-        write(f, json_string)
+media_op = [0.05, 0.5, 0.95]
+mos = join([string(el) for el in media_op], ";", ";")
+
+# Example Media
+for pₘ in [0.1, 0.2, 0.3, 0.4, 0.5]
+    for ϵ in [0.1, 0.2, 0.3, 0.4, 0.5, 1.0]
+        for γ in [0.0, 0.5, 0.75, 1, 1.25, 1.5]
+            params = [g, ϵ, γ, γ, pₘ, media_op, max_t]
+            name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t"
+            final_opinions, final_clusters, final_its =  multiple_runs(deffuant_bias_media, name, params, nsteady; nruns=100)
+            write_aggregate(name, final_opinions, final_clusters, final_its)
+            paramvalues = "$n $p $ϵ $γ $γ $pₘ $max_t $mos"
+            writeaverages(name, paramvalues, n)
+        end
     end
 end
 
-function multiple_runs(f, name, params, nsteady; nruns)
-    finalops = []
-    final_clusters = []
-    its = []
-    println("starting $nruns runs for params = $params")
-    for nr in 1:nruns
-        r =  f(params... ; nsteady=nsteady)
-        df = DataFrame(r)
-        CSV.write("res/$name nr$nr.csv",  df, header=false)
-        spaghetti_plot(df, size(r)[1], "plots/$name nr$nr.png")
-        append!(its,size(r)[1])
-        # aggregate stats
-        fo = r[size(r)[1]]
-        ϵ = params[2]
-        clusters = population_clusters([x for x in r[end]], ϵ)
-        append!(final_clusters, clusters)
-        append!(finalops, [fo])
-    end
-    itsfile = open("aggregate/num it $name.txt", "w")
-    avg_niter = sum(its)/size(its)[1]
-    write(itsfile, "$avg_niter")
-    close(itsfile)
-    return finalops, final_clusters
-end
+
+
+
+
+
+
+
+
+
+
+
+
+# # Example Deffuant
+# for ϵ in [0.1, 0.2, 0.3, 0.4]
+#     params = [g, ϵ, max_t]
+#     name = "deffuant e$ϵ mi$max_t"
+#     final_opinions, final_clusters = multiple_runs(deffuant, name, params, nsteady; nruns=10)
+#     write_aggregate(name, final_opinions, final_clusters)
+# end
+
+
+# # Example Bias
+# for ϵ in [0.1, 0.2, 0.3, 0.4], γ in [0, 1, 1.5, 2]
+#     name = "bias e$ϵ g$γ mi$max_t"
+#     params = [g, ϵ, γ, max_t]
+#     final_opinions, final_clusters = multiple_runs(deffuant_bias, name, params, nsteady; nruns=10)
+#     write_aggregate(name, final_opinions, final_clusters)
+# end
