1d3d6fab4272b6dc73ac30e422c0d0dd61a72da1
ValentinaPansanella <valentina.pansanella@gmail.com>
2021-08-24 05:43:10 +0200

    added check for file existence and corrected csv header

--- analysis.jl
+++ analysis.jl
@@ -9,65 +9,67 @@ function keys_to_int(dict)
 end
 
 function nclusters(name, n)
-    open("aggregate/final_clusters $name.json", "r") do f
-        global filedict
-        filedict = JSON.parse(f)  # parse and transform data
-    end
-    clusters = keys_to_int(filedict)
-    ncarray = []
-    for nr in keys(clusters)
-        C_num = n*n
-        C_den = 0
-        for k in keys(clusters[nr])
-            C_den += clusters[nr][k]*clusters[nr][k]
+    if isfile("aggregate/final_clusters $name.json")
+        open("aggregate/final_clusters $name.json", "r") do f
+            global filedict
+            filedict = JSON.parse(f)  # parse and transform data
         end
-        C = C_num / C_den
-        append!(ncarray, [C])
+        clusters = keys_to_int(filedict)
+        ncarray = []
+        for nr in keys(clusters)
+            C_num = n*n
+            C_den = 0
+            for k in keys(clusters[nr])
+                C_den += clusters[nr][k]*clusters[nr][k]
+            end
+            C = C_num / C_den
+            append!(ncarray, [C])
+        end
+        f = open("aggregate/enc $name.csv", "w")
+        s = join([string(el) for el in ncarray], ",")
+        write(f, s)
+        close(f)
+        return ncarray
     end
-    f = open("aggregate/enc $name.csv", "w")
-    s = join([string(el) for el in ncarray], ",")
-    write(f, s)
-    close(f)
-    return ncarray
 end
 
 function pwdists(name, n)
-    open("aggregate/final_opinions $name.json", "r") do f
-        global filedict
-        filedict = JSON.parse(f)  # parse and transform data
-    end
-    opinions = keys_to_int(filedict)
-    
-    pwdarray = zeros(Float16, length(opinions), n, n)
-    for nr in keys(opinions)
-        for i in 1:n
-            o_i = opinions[nr][i]
-            for j in i:n
-                o_j = opinions[nr][j]
-                d = abs.(o_i-o_j)
-                pwdarray[nr, i, j] = d
+    if isfile("aggregate/final_opinions $name.json")
+        open("aggregate/final_opinions $name.json", "r") do f
+            global filedict
+            filedict = JSON.parse(f)  # parse and transform data
+        end
+        opinions = keys_to_int(filedict)
+        
+        pwdarray = zeros(Float64, length(opinions), n, n)
+        for nr in keys(opinions)
+            for i in 1:n
+                o_i = opinions[nr][i]
+                for j in i:n
+                    o_j = opinions[nr][j]
+                    d = abs.(o_i-o_j)
+                    pwdarray[nr, i, j] = d
+                end
             end
         end
+        return pwdarray
     end
-    # f = open("aggregate/pwdss $name.csv", "w")
-    # s = join([string(mean(el)) for row in pwdarray], ",")
-    # write(f, s)
-    # close(f)
-    return pwdarray
 end
 
 function nits(name)
-    open("aggregate/final_iterations $name.json", "r") do f
-        global filedict
-        filedict = JSON.parse(f)  # parse and transform data
+    if isfile("aggregate/final_iterations $name.json")
+        open("aggregate/final_iterations $name.json", "r") do f
+            global filedict
+            filedict = JSON.parse(f)  # parse and transform data
+        end
+        iterations = keys_to_int(filedict)
+        
+        itarray = []
+        for nr in keys(iterations)
+            append!(itarray, iterations[nr])
+        end
+        return itarray
     end
-    iterations = keys_to_int(filedict)
-    
-    itarray = []
-    for nr in keys(iterations)
-        append!(itarray, iterations[nr])
-    end
-    return itarray
 end
 
 function mean_and_std(array)
@@ -84,11 +86,9 @@ function writeaverages(name, paramvalues, n)
     string = paramvalues * " $avgnc $stdnc $avgpwd $stdpwd $avgnits $stdnits"
     list = split(string)
     s = join(list, ", ")
-    header = "n,eps,gam,gam_media,p_media,max_it,media_ops,avg_nc,std_nc,avg_pwdist,std_pwdist,avg_niter,std_niter\n"
+    header = "n,p,eps,gam,gam_media,p_media,max_it,media_ops,avg_nc,std_nc,avg_pwdist,std_pwdist,avg_niter,std_niter\n"
     f = open("aggregate/averages $name.csv", "w")
     write(f, header)
     write(f, s)
     close(f)
-        
-        
 end
