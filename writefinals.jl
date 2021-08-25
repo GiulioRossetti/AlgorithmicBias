4dbd4057de968ca7c9abee54c34fa7b74f2c728c
ValentinaPansanella <valentina.pansanella@gmail.com>
2021-08-25 16:46:11 +0200

    writecsv.jl renamed in writefinals.jl

--- writefinals.jl
+++ writefinals.jl
@@ -18,22 +18,12 @@ g = erdos_renyi(n, p)
 max_t = 1000000
 nsteady = 1000
 
-media_op = [0.05, 0.5, 0.95]
+media_op = [0.00]
 mos = join([string(el) for el in media_op], ";", ";")
 
 #Writing averages csv
-for pₘ in [0.1, 0.2, 0.3]
-    for ϵ in [0.1, 0.2, 0.3, 0.4, 0.5, 1.0]
-        for γ in [0.0, 0.25, 0.5, 0.75, 1.25, 1.5]
+for pₘ in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0], ϵ in [0.1, 0.2, 0.3, 0.4, 0.5, 1.0], γ in [0.0, 0.25, 0.5, 0.75, 1.25, 1.5]
             name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t"
             paramvalues = "$n $p $ϵ $γ $γ $pₘ $max_t $mos"
-            if isfile("aggregate/averages $name.csv")
-                continue
-            else
-                if isfile("aggregate/final_clusters $name.json")
-                    writeaverages(name, paramvalues, n)
-                end
-            end
-        end
-    end
+            isfile("aggregate/averages $name.csv") ? continue : if isfile("aggregate/final_clusters $name.json") ? writeaverages(name, paramvalues, n) : continue            
 end
