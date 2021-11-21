aa0a4636249f972ea4fad01974a9022ebca7467f
Valentina Pansanella <valentina@alina.di.unipi.it>
2021-11-21 18:08:02 +0100

    files one media

--- onemedia.jl
+++ onemedia.jl
@@ -36,31 +36,3 @@ for pₘ in [0.0, 0.1, 0.3, 0.5], ϵ in [0.2, 0.3, 0.5], γ in [0.0, 0.25, 0.5, 
         writeaverages(name, params, n, p)
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-# # Example Deffuant
-# for ϵ in [0.1, 0.2, 0.3, 0.4]
-#     params = [g, ϵ, max_t]
-#     name = "deffuant e$ϵ mi$max_t"
-#     final_opinions, final_clusters = multiple_runs(deffuant, name, params, nsteady; nruns=10)
-#     write_aggregate(name, final_opinions, final_clusters)
-# end
-
-
-# # Example Bias
-# for ϵ in [0.2], γ in [0, 1, 1.5, 2]
-#     name = "bias e$ϵ g$γ mi$max_t"
-#     params = [g, ϵ, γ, max_t]
-#     final_opinions, final_clusters, final_its = multiple_runs(deffuant_bias, name, params, nsteady; nruns=1)
-#     write_aggregate(name, final_opinions, final_clusters, final_its)
-# end
