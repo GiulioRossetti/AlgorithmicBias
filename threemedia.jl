a4480f6280c20f53af2c1d4ad46f1e67704427bf
ValentinaPansanella <valentina.pansanella@gmail.com>
2021-12-07 00:10:55 +0100

    updates

--- threemedia.jl
+++ threemedia.jl
@@ -19,7 +19,7 @@ g = erdos_renyi(n, p)
 # simulation global parameters
 max_t = 1000000
 nsteady = 1000
-nruns = 15
+nruns = 10
 
 # Prova file che non esiste -> nruns tutte ancora da fare, non ci sono res file, non ci sono aggregate file: sembrerebbe ok
 
@@ -49,12 +49,12 @@ nruns = 15
 #     final_opinions, final_clusters, final_its = return_dictionaries(f, name, params; nruns)
 #     write_aggregate(name, final_opinions, final_clusters, final_its)
 
-#Make plots
-for pₘ in [0.1], ϵ in [0.2], γ in [0.7]
-    media_op = [0.0]
-    mos = join([string(el) for el in media_op], ";", ";")
-    println("plotting process started")
-    params = [g, ϵ, γ, γ, pₘ, media_op, max_t]
-    name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t"
-    plotting(name; nruns)
-end
+#Make plots: forse i plot meglio farli dopo con python che mi vengono meglio
+# for pₘ in [0.1], ϵ in [0.2], γ in [0.7]
+#     media_op = [0.0]
+#     mos = join([string(el) for el in media_op], ";", ";")
+#     println("plotting process started")
+#     params = [g, ϵ, γ, γ, pₘ, media_op, max_t]
+#     name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t"
+#     plotting(name; nruns)
+# end
