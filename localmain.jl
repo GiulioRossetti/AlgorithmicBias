b6b87aa8ff7a65a8f0458e6e370ce761e77d3fcd
valen <valentina.pansanella@gmail.com>
2022-04-22 15:17:35 +0200

    updates

--- localmain.jl
+++ localmain.jl
@@ -21,25 +21,25 @@ max_t = 1000000
 nsteady = 1000
 nruns = 100
 
-for pₘ in [0.0], ϵ in [0.2, 0.3, 0.4, 0.5], γ in [0.0, 0.5, 0.75, 1.0, 1.25, 1.5]
+for pₘ in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5], ϵ in [0.2, 0.3, 0.4, 0.5], γ in [0.0, 0.5, 0.75, 1.0, 1.25, 1.5]
     f = deffuant_bias_media
-    media_op = [0.5]
+    media_op = [0.05, 0.5, 0.95]
     mos = join([string(el) for el in media_op], ";", ";")
     params = [g, ϵ, γ, γ, pₘ, media_op, max_t]
     name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t"
-    # # multiple_runs(f, name, params, nsteady; nruns)
-    try
-        if isfile("aggregate/final_iterations $name.json") == false
-            println("aggregate/final_iterations $name.json")
-            # final_opinions, final_clusters, final_its = return_dictionaries(f, name, params; nruns)
-            # write_aggregate(name, final_opinions, final_clusters, final_its)
-            # writeaverages(name, params, mos, n, p)
-        end
-    catch e
-        println(e)
-        continue
-    end
-    # writeaverages(name, params, mos, n, p)
+    multiple_runs(f, name, params, nsteady; nruns)
+    # try
+    #     if isfile("aggregate/final_iterations $name.json") == false
+    #         println("aggregate/final_iterations $name.json")
+    #         # final_opinions, final_clusters, final_its = return_dictionaries(f, name, params; nruns)
+    #         # write_aggregate(name, final_opinions, final_clusters, final_its)
+    #         # writeaverages(name, params, mos, n, p)
+    #     end
+    # catch e
+    #     println(e)
+    #     continue
+    # end
+    # # writeaverages(name, params, mos, n, p)
 
 end
 
