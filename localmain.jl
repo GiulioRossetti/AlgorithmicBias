465bc4e18c2be161889ef6882db065f43e79df5c
ValentinaPansanella <valentina.pansanella@gmail.com>
2022-10-11 15:22:21 +0200

    small updates

--- localmain.jl
+++ localmain.jl
@@ -21,9 +21,9 @@ max_t = 1000000
 nsteady = 1000
 nr=1
 
-for media_op in [[0.05, 0.95]], pₘ in [0.5], ϵ in [0.32], γ in [1.6]
+for media_op in [[0.05, 0.5, 0.95], [0.05, 0.95], [0.0], [0.5]], pₘ in [0.5, 0.4, 0.3, 0.2, 0.0], ϵ in [0.2, 0.3, 0.4, 0.5], γ in [0.0, 0.5, 0.75, 1.0, 1.25, 1.5]
     f = deffuant_bias_media
     params = [g, ϵ, γ, γ, pₘ, media_op, max_t]
-    name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t new new"
+    name = "media mo$media_op p$pₘ e$ϵ g$γ gm$γ mi$max_t"
     single_run(f, name, params, nsteady; nr)
 end
